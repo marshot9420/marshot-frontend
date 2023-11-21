b89ea99276c5151081471230b21b9d44e41c7c38
marshot <124557537+marshot9420@users.noreply.github.com>
2023-11-21 23:03:03 +0900

    [Deps] React Query 의존성 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CreateDOM from 'react-dom/client';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 
 import { ThemeProvider } from '@emotion/react';
@@ -10,16 +11,19 @@ import App from '@/App';
 import { GlobalStyles, theme } from '@/styles';
 
 const root = document.getElementById('root');
+const queryClient = new QueryClient();
 
 if (!root) throw new Error('root를 찾을 수 없습니다.');
 
 const appRoot = CreateDOM.createRoot(root);
 
 appRoot.render(
-  <ThemeProvider theme={theme}>
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <GlobalStyles />
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
-  </ThemeProvider>,
+  </QueryClientProvider>,
 );
