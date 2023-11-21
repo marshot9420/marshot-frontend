26ff81f7cf72a4ebc3c69659c9d4ed07fb626d34
marshot <124557537+marshot9420@users.noreply.github.com>
2023-11-21 19:07:47 +0900

    [Configs] webpack-dev-server 설정 추가

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,25 @@
-import webpack, { Configuration } from 'webpack';
+import webpack, { Configuration as WebpackConfig } from 'webpack';
 import path from 'path';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { Configuration as DevServerConfig } from 'webpack-dev-server';
+import dotenv from 'dotenv';
+
+interface IConfiguration extends WebpackConfig {
+  devServer?: DevServerConfig;
+}
 
 const DEV_ENV = 'development';
 const PROD_ENV = 'production';
 const isDevMode = process.env.NODE_ENV !== PROD_ENV;
 
+dotenv.config({ path: isDevMode ? '.env.development' : '.env.production' });
+
+const PORT = process.env.PORT;
+
 const OUTPUT_DIR = 'dist';
 
-const config: Configuration = {
+const config: IConfiguration = {
   name: 'marshot-frontend',
   mode: isDevMode ? DEV_ENV : PROD_ENV,
   devtool: isDevMode ? 'inline-source-map' : 'hidden-source-map',
@@ -30,6 +40,11 @@ const config: Configuration = {
   output: {
     path: path.join(__dirname, OUTPUT_DIR),
     filename: '[name].js',
+  },
+  devServer: {
+    historyApiFallback: false,
+    port: PORT,
+    devMiddleware: { publicPath: OUTPUT_DIR },
   },
 };
 
