a9b81c03d32b95818976c5caa755b25221fc725c
marshot <124557537+marshot9420@users.noreply.github.com>
2024-02-11 18:12:32 +0900

    refactor: 레이아웃 재구성

---
+++ js/favorites.mjs
@@ -0,0 +1,3 @@
+const favoriteForm = document.getElementById("favorite-form");
+const favoriteName = document.getElementById("favorite-name");
+const favoriteUrl = document.getElementById("favorite-url");
