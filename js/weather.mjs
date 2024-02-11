0e83be5cee5626995b241a3b72ff2509fca694d7
marshot <124557537+marshot9420@users.noreply.github.com>
2024-02-11 15:59:58 +0900

    feat: 날씨 API 가져와서 출력하는 로직 추가

---
+++ js/weather.mjs
@@ -0,0 +1,27 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "8b959aeb5a5640bf8e98f009d71b825a";
+
+function onGeolocationSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  console.log("Latitude: ", latitude);
+  console.log("Longitude: ", longitude);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("데이터: ", data.main);
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeolocationError() {
+  alert("위치 정보를 찾을 수 없습니다.");
+}
+
+navigator.geolocation.getCurrentPosition(
+  onGeolocationSuccess,
+  onGeolocationError
+);
