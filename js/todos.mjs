95f428d3905b11814e4adbb1bc415c48d9115d0c
marshot <124557537+marshot9420@users.noreply.github.com>
2024-02-11 15:33:34 +0900

    feat: 투두리스트 삭제 로직 추가

--- js/todos.mjs
+++ js/todos.mjs
@@ -10,6 +10,13 @@ function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   li.id = newToDo.id;
@@ -17,14 +24,19 @@ function paintToDo(newToDo) {
   const span = document.createElement("span");
   span.innerText = newToDo.text;
 
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+
   li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
 
-  const newToDo = toDoInput.ariaValueMax;
+  const newToDo = toDoInput.value;
 
   toDoInput.value = "";
 
@@ -34,6 +46,7 @@ function handleToDoSubmit(event) {
   };
 
   toDos.push(newToDoObject);
+
   paintToDo(newToDoObject);
   saveToDos();
 }
